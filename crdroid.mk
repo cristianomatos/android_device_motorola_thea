cc30989dca91dd7099f67f62c91eeceacf1a2bc8
Shawn5162 <wahi.shawn@gmail.com>
2015-07-03 12:41:59 -0400

    Updated crdroid.mk

--- crdroid.mk
+++ crdroid.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/motorola/falcon/full_thea.mk)
+$(call inherit-product, device/motorola/thea/full_thea.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G 2014 LTE
 PRODUCT_NAME := crdroid_thea
